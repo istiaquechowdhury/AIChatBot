8db9515cc0a4fa1e75323135588be7a85a3aa58e
istiaquechowdhury <istiaquechowdhury430@gmail.com>
2025-05-31 19:35:39 +0600

    code clean up

--- AIChatBot.Web/Service/TavilyService.cs
+++ AIChatBot.Web/Service/TavilyService.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text;
 
 namespace AIChatBot.Web.Service
@@ -16,32 +15,7 @@ namespace AIChatBot.Web.Service
             Console.WriteLine($"Using API Key: {_apiKey}");
         }
 
-        //public async Task<string> GetAnswerAsync(string question)
-        //{
-        //    var requestData = new
-        //    {
-        //        query = question
-        //    };
-
-        //    var content = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
-
-        //    _httpClient.DefaultRequestHeaders.Clear();
-        //    _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
-
-        //    var response = await _httpClient.PostAsync("https://api.tavily.com/search", content);
-
-        //    if (!response.IsSuccessStatusCode)
-        //        return "Sorry, I couldn't respond right now.";
-
-        //    var responseJson = await response.Content.ReadAsStringAsync();
-        //    Console.WriteLine("Tavily API Response: " + responseJson);
-
-        //    using var doc = JsonDocument.Parse(responseJson);
-        //    if (doc.RootElement.TryGetProperty("answer", out var answerElement))
-        //        return answerElement.GetString() ?? "No answer received.";
-
-        //    return "No answer received.";
-        //}
+       
         public async Task<string> GetAnswerAsync(string question)
         {
             var requestData = new
